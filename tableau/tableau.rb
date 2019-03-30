f0cd463458fb740007f1e40522e4ba93d03ef68e
bamss <amadougolfa98@gmail.com>
2019-03-30 16:49:06 +0100

    les exos array done

--- tableau/tableau.rb
+++ tableau/tableau.rb
@@ -121,4 +121,116 @@ def tabsum
     puts result
 end
 
-tabsum
+# tabsum
+
+def tow_tab_sum
+    tab1 = [4, 8, 7, 9, 1, 5, 4, 6]
+    tab2 = [7, 6, 5, 2, 1, 3, 7, 4]
+    tab3 = []
+    i = 0
+    while i < tab1.size
+        result = tab1[i] + tab2[i]
+        tab3 << result
+        i += 1
+    end
+    p tab3
+end
+# tow_tab_sum
+
+def two_tab_produit
+    tab1 = [4, 8, 7, 12]
+    tab2 = [3, 6]
+    sum = 0
+    # i = 0
+    # while i < tab1.size
+    #     result = tab1[i] * tab2[0]
+    #     result1 = tab1[i] * tab2[1]
+    #     sum += result + result1
+    #     i += 1
+    # end
+    # puts sum
+    for i in 0..tab1.size - 1
+        for j in 0..tab2.size - 1
+          p  sum = sum + tab1[i] * tab2[j]
+        end
+    end
+    # puts "somme = #{sum}"
+
+end
+
+# two_tab_produit
+
+def plus_one
+    print "rentrez le nombre d'élément que vous voulez dans votre tableau: "
+    nb = gets.chomp.to_i
+    tab = []
+    i = 0
+    while i < nb
+        print "rentrez le nombre: "
+        n = gets.chomp.to_i + 1
+        tab << n
+        i += 1
+    end
+    p tab
+end
+
+# plus_one
+
+def bigger
+    print "rentrez le nombre d'élément que vous voulez dans votre tableau: "
+    nb = gets.chomp.to_i
+    tab = []
+    i = 0
+    pg = 0
+    index = 0
+    while i < nb
+        print "rentrez le nombre: "
+        n = gets.chomp.to_i
+        tab << n
+        i += 1
+    end
+
+    for i in 0..tab.size - 1
+        if tab[i] > pg
+            pg = tab[i]
+            index = i
+        end
+    end
+    puts "#{pg} est le plus grand nombre du tableau"
+    puts "et il à l'indexe #{index}"
+end
+
+# bigger
+
+def toto_classe
+    print "rentrez le nombre d'éléves de votre classe: "
+    nb = gets.chomp.to_i
+    nbsup = 0
+    sum = 0
+    tab = []
+
+    for i in 0..nb
+        puts "Entrez le nombre n° #{i}"
+        note = gets.chomp.to_i
+        tab << note
+    end
+
+    for i in 0..tab.size - 1
+        sum = sum + tab[i]
+    end
+
+    moy = sum / nb
+    for i in 0..nb - 1
+        if tab[i] > moy
+            nbsup = nbsup + 1
+        end
+    end
+    p moy
+    puts "#{nbsup} élèves dépassen la moyenne de la classe"
+end
+
+toto_classe
+
+
+
+
